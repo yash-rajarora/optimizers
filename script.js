425edd4e972b82ccc8c8d0d0e8ae81a6316932ff
supriyaaaaa7 <113242343+supriyaaaaa7@users.noreply.github.com>
2022-09-17 17:49:16 +0530

    ghgds

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-// $("#search-icon").click(function() {
-//   $(".nav").toggleClass("search");
-//   $(".nav").toggleClass("no-search");
-//   $(".search-input").toggleClass("search-active");
-// });
-
-// $('.menu-toggle').click(function(){
-//    $(".nav").toggleClass("mobile-nav");
-//    $(this).toggleClass("is-active");
-// });
+var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+      invert: false,
+    },
+    // autoHeight: true,
+    pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true,
+    }
+  });
